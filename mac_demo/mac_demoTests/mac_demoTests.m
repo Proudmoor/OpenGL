a0472f81cb6234676c93e1636d464be4d2834ae9
zpf <pengfei@zhangpengfeideMacBook-Air.local>
2014-09-20 13:45:55 +0800

    mac_demo

---
+++ mac_demo/mac_demoTests/mac_demoTests.m
@@ -0,0 +1,34 @@
+//
+//  mac_demoTests.m
+//  mac_demoTests
+//
+//  Created by zpf on 14-9-20.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface mac_demoTests : XCTestCase
+
+@end
+
+@implementation mac_demoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
