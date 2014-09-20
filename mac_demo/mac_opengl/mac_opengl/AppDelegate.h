c7824611ed762c329665104f6c9a694b9c4c9147
zpf <pengfei@zhangpengfeideMacBook-Air.local>
2014-09-20 20:31:37 +0800

    mac_demo

---
+++ mac_demo/mac_opengl/mac_opengl/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  mac_opengl
+//
+//  Created by zpf on 14-9-20.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ mac_demo/mac_opengl/mac_opengl/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  mac_opengl
+//
+//  Created by zpf on 14-9-20.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
