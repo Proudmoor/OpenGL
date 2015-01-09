4446ab44d54a830b3dbfdad2c8c7c3979d34f444
Proudmoor <zpf238@gmail.com>
2015-01-09 23:59:35 +0800

    a

---
+++ A_SDL_Example/Lesson1/src/Init.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include <SDL2/SDL.h>
+#include "res_path.h"
+
+int main (int argc, char **argv) {
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		std::cerr << "SDL_Init error: " <<SDL_GetError() << std::endl;
+		return 1;
+	}
+
+	SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+	if (win == nullptr){
+		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+	}
+
+	SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (ren == nullptr) {
+		SDL_DestroyWindow(win);
+		std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+
+	std::string imagePath = getResourcePath("Lesson1") + "image.bmp";
+	SDL_Surface *bmp = SDL_LoadBMP(imagePath.c_str());
+	if (bmp == nullptr) {
+		SDL_DestroyRenderer(ren);
+		SDL_DestroyWindow(win);
+		std::cout << "SDL_LoadBMP Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+
+	SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, bmp);
+	SDL_FreeSurface(bmp);
+	if(tex == nullptr) {
+		SDL_DestroyRenderer(ren);
+		SDL_DestroyWindow(win);
+		std::cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+	std::cout << "Resource path is: " << getResourcePath() << std::endl;
+
+	SDL_RenderClear(ren);
+	SDL_RenderCopy(ren, tex, NULL, NULL);
+	SDL_RenderPresent(ren);
+	SDL_Delay(2000);
+
+	SDL_Quit();
+	return 0;
+}
